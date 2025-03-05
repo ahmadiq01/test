f0f399e32d7de768af81a916fdbb086d8852ea8c
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-03-05 12:17:48 +0500

    pdated the svg of the our story

--- src/pages/story/story.jsx
+++ src/pages/story/story.jsx
@@ -11,31 +11,31 @@ const ContactPage = () => {
         <div className="relative bg-white">
             <Header />
             
-                <div className="container mx-auto px-6 lg:px-20 mt-10 relative">
+                <div className="container mx-auto px-6 lg:px-20 mt-10 relative bg-amber-950 ">
 
-                <div className="mt-[10px] bg-red-800">
+                <div className="">
                     <img 
                         src={V1} 
                         alt="Vector 1" 
-                        className="absolute w-[284px] h-[284px] top-[1250px] left-[9200px] rotate-0"
+                        className="absolute w-[284px] h-[94px] top-[174px] left-[950px] -rotate-9"
                     />
                     <img 
                         src={V2} 
                         alt="Vector 2" 
-                        className="absolute w-[284px] h-[284px] top-[422px] left-[1489px] -rotate-83"
+                        className="absolute w-[284px] h-[64px] top-[170px] left-[1150px] rotate-19"
                     />
                     <img 
                         src={V3} 
                         alt="Vector 3" 
-                        className="absolute w-[14px] h-[135px] top-[787px] left-[1170px]"
+                        className="absolute w-[284px] h-[84px] top-[320px] left-[1070px] rotate-6"
                     />
                 </div>
 
                 {/* Main Content */}
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 relative z-10">
                     {/* Left Column - Story Text */}
-                    <div>
-                        <div className="mb-12">
+                    <div className=' bg-red-600'>
+                        <div className="mb-12 ">
                             <h1 className="text-9xl font-bold tracking-[0.5rem] uppercase font-[Heathergreen]">Our Story</h1>
                         </div>
                         <p className="text-lg text-gray-700 leading-relaxed mb-6">
@@ -69,7 +69,7 @@ const ContactPage = () => {
                             </div>
 
                             {/* Team Member: Sajjad */}
-                            <div className="flex items-center flex-col self-end space-y-3 mr-[20px] mt-[10px]">
+                            <div className="flex items-center flex-col self-end space-y-3 mr-[20px] mt-[-40px]">
                                 <div className="w-32 h-32 rounded-full border-4 border-blue-500 overflow-hidden">
                                     <img 
                                         src="https://s3-alpha-sig.figma.com/img/2517/76a7/c47934b95505556a5d40dfcc24d3aee2?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=g2xGkrFKR1GNkF-ZnlINHifQuDPBJqwR4wxxMUnrbe3spAbkOlLwkjRDwKTM78RX6ZBt4hviv2ClMfPahruWOyk10~fw-lrA087rGMnBjdMvn~UpvpKOQEmHSYHE1hPDcSA8ud0-ym59lPZV5JiXmaBHGIhv18k4K15DJlcGnNjEapm8jm61cJlhjPqBjiHJLr0~fP0QHFMdBaJV53pN1J1LO6MBBuN3f7d2I4EYTGiAKDpLxbHNF1SsdzX2QhpVfMAEl1fTXRczjFrZpC~RYdT8S7GJoM9YV5lRsYJkc2pGO4KVaCliMTB6gbHDINPRwoE7jiBNv18-cxuhTJiCzA__" 
@@ -85,7 +85,7 @@ const ContactPage = () => {
 
 
                             {/* Team Member: Faizan */}
-                            <div className="flex  mr-[108px] mt-[-140px] mb-[10px] items-center flex-col col-1 space-x-6">
+                            <div className="flex  mr-[108px] mt-[-175px] mb-[10px] items-center flex-col col-1 space-x-6">
                                 <div className="w-32 h-32 rounded-full border-4 border-green-500 overflow-hidden">
                                     <img 
                                         src="https://s3-alpha-sig.figma.com/img/7f21/5f05/844c0e7d065012026c1c112ec07a03be?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FanQGBhdTQJnz2NkLzmlwl0MVbZjEbP5kPllZ~4cx4rh7FiNuHxVkIr6dW1SAyyjh3AvlJ1Ogs1I4Yrk-wzqRRfZ4Rxe1kmXmjId7V8A9AHur7BBHzqApnDAe2HtkZ~zwRaA9Y6kbQ9EUnHQN8SDSJPhchL50GFrUwRBV2QZ~hbf7-PuIW3qcYupuuGwkQ80PqdCmHk-bPhcoSBLT8c4wy10ylqZ8~1AS~uLTKAiAVcibYb1284YHa71vdvc4Oqe~Cg1oAQkd7F-8T~NXZ-4lOaeVCLmsWzrkL~qX1gMQV6JsG~xUd4wjrveANC38RuuEJv7amKKFs7mxG~bgXus1A__" 
