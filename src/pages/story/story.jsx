7cc79ba4567c79a5134b37a47f73c12f5124d76d
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-03-07 19:40:56 +0500

    Final Updates

--- src/pages/story/story.jsx
+++ src/pages/story/story.jsx
@@ -27,7 +27,7 @@ const ContactPage = () => {
             <img
               src={V2}
               alt="Vector 2"
-              className="absolute w-[142px] h-[45px] top-[338px] left-[215px] rotate-[9.5deg] md:w-[284px] md:h-[65px] md:top-[180px] md:left-[190px] md:rotate-[12deg]"
+              className="absolute w-[142px] h-[45px] top-[324px] left-[215px] rotate-[9.5deg] md:w-[284px] md:h-[65px] md:top-[180px] md:left-[190px] md:rotate-[12deg]"
             />
             <img
               src={V3}
@@ -51,7 +51,7 @@ const ContactPage = () => {
               <div className="w-full">
                 <div className="flex flex-col items-center space-y-8 w-full md:mt-[-220px]">
                     
-                  <div className="flex items-center flex-col space-x-6 md:mt-[10px] mr-[-120px] mb-[-50px] md:ml-[30px]">
+                  <div className="flex items-center flex-col space-x-6 md:mt-[10px] mr-[-150px] mb-[-50px] md:ml-[30px]">
                     <div className="md:w-55 md:h-55 w-32 h-32 rounded-full border-4 border-yellow-400 overflow-hidden">
                       <img
                         src="https://s3-alpha-sig.figma.com/img/cd4b/18e8/6e997e80693617249606bd8795db8ec0?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=L1OMp1YIJy5lKxwGQXm8L-6mJgwvD7kGYHnyT7~-GPvO2qUR-mJnUpfqrcBvzZuikmXGBXJgja25uedfiPgTpBNkw6PONI9fxKdQ~2rIDKkhzHOmuN5kVuddDGB0-KaHeHEfAPQJqDDXjoRaCXBQXbGFClLqj2juiypQlFfzajzuXbaKvQkuUSrojZmqhpZI2R6DMg25W02X0rqccvRzaD~rKBnUUwukdKL4YF8dMTq3v5Jk3FpOlY6iU~FZ~15IFhKuJEbjBYpWxcPIqKYbvSAl5I6gGh-GdYPMu7ybQLBetvAP2Za29Q3V-TxMBOrEWB4O2d2xmzMv3Up4FHxkOw__"
@@ -65,7 +65,7 @@ const ContactPage = () => {
                     </div>
                   </div>
 
-                  <div className="flex items-center flex-col self-end space-y-3 md:mr-[-250px] mr-[-90px] md:mt-[-10px]">
+                  <div className="flex items-center flex-col self-end space-y-3 md:mr-[-250px] mt-[-10px] mr-[-85px] md:mt-[-10px]">
                     <div className="md:w-55 md:h-55 w-32 h-32  rounded-full border-4 border-blue-500 overflow-hidden">
                       <img
                         src="https://s3-alpha-sig.figma.com/img/2517/76a7/c47934b95505556a5d40dfcc24d3aee2?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=g2xGkrFKR1GNkF-ZnlINHifQuDPBJqwR4wxxMUnrbe3spAbkOlLwkjRDwKTM78RX6ZBt4hviv2ClMfPahruWOyk10~fw-lrA087rGMnBjdMvn~UpvpKOQEmHSYHE1hPDcSA8ud0-ym59lPZV5JiXmaBHGIhv18k4K15DJlcGnNjEapm8jm61cJlhjPqBjiHJLr0~fP0QHFMdBaJV53pN1J1LO6MBBuN3f7d2I4EYTGiAKDpLxbHNF1SsdzX2QhpVfMAEl1fTXRczjFrZpC~RYdT8S7GJoM9YV5lRsYJkc2pGO4KVaCliMTB6gbHDINPRwoE7jiBNv18-cxuhTJiCzA__"
@@ -79,7 +79,7 @@ const ContactPage = () => {
                     </div>
                   </div>
 
-                  <div className="flex md:mr-[190px] md:mt-[-225px] md:mb-[0px] mr-[108px] mt-[-125px] mb-[20px] items-center flex-col space-x-6">
+                  <div className="flex md:mr-[190px] md:mt-[-225px] md:mb-[0px] mr-[06px] mt-[-125px] mb-[20px] items-center flex-col space-x-6">
                     <div className="md:w-55 md:h-55 w-32 h-32  rounded-full border-4 border-green-500 overflow-hidden">
                       <img
                         src="https://s3-alpha-sig.figma.com/img/7f21/5f05/844c0e7d065012026c1c112ec07a03be?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FanQGBhdTQJnz2NkLzmlwl0MVbZjEbP5kPllZ~4cx4rh7FiNuHxVkIr6dW1SAyyjh3AvlJ1Ogs1I4Yrk-wzqRRfZ4Rxe1kmXmjId7V8A9AHur7BBHzqApnDAe2HtkZ~zwRaA9Y6kbQ9EUnHQN8SDSJPhchL50GFrUwRBV2QZ~hbf7-PuIW3qcYupuuGwkQ80PqdCmHk-bPhcoSBLT8c4wy10ylqZ8~1AS~uLTKAiAVcibYb1284YHa71vdvc4Oqe~Cg1oAQkd7F-8T~NXZ-4lOaeVCLmsWzrkL~qX1gMQV6JsG~xUd4wjrveANC38RuuEJv7amKKFs7mxG~bgXus1A__"
--- src/pages/subscribe/subscribe.jsx
+++ src/pages/subscribe/subscribe.jsx
@@ -125,7 +125,7 @@ const Home = () => {
       {/* Hero Section */}
       <div className=" flex flex-col items-center justify-center text-center py-10">
         <div className="md:w-full font-[Heathergreen] font-normal flex flex-col items-center">
-          <p className="font-semibold text-9xl md:text-[150px] md:leading-[50px] leading-[50px] tracking-[2px]">
+          <p className="font-semibold text-8xl md:text-[150px] md:leading-[50px] leading-[50px] tracking-[2px]">
             Find the Right Plan
           </p>
         </div>
