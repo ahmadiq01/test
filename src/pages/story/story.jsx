2d5cebb13ae08ea728e000ef385720602999d97c
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-03-11 19:41:43 +0500

    Update story.jsx

--- src/pages/story/story.jsx
+++ src/pages/story/story.jsx
@@ -27,7 +27,7 @@ const ContactPage = () => {
                 alt="Vector 1"
                 className="
                 absolute 
-                w-[142px] h-[75px] top-[128px] left-[9px] rotate-[-6.5deg] 
+                w-[142px] h-[75px] top-[118px] left-[9px] rotate-[-6.5deg] 
                 md:w-[84px] md:h-[100px] md:top-[315px] md:left-[-35px] md:rotate-[-4deg]
                 lg:w-[84px] lg:h-[100px] lg:top-[215px] lg:left-[-25px] lg:rotate-[-4deg]
                 "
@@ -37,7 +37,7 @@ const ContactPage = () => {
                 alt="Vector 2"
                 className="
                 absolute 
-                w-[142px] h-[45px] top-[75px] left-[206px] rotate-[16deg] 
+                w-[142px] h-[45px] top-[89px] left-[206px] rotate-[16deg] 
                 md:w-[284px] md:h-[65px] md:top-[180px] md:left-[190px] md:rotate-[12deg]
                 lg:w-[284px] lg:h-[65px] lg:top-[180px] lg:left-[190px] lg:rotate-[12deg]
                 "
@@ -46,7 +46,7 @@ const ContactPage = () => {
                 src={V3}
                 alt="Vector 3"
                 className="
-                absolute w-[132px] h-[50px] top-[270px] left-[130px] rotate-[3deg]
+                absolute w-[122px] h-[50px] top-[280px] left-[130px] rotate-[3deg]
                 md:w-[284px] md:h-[104px] md:top-[435px] md:left-[89px] md:rotate-[6deg]
                 lg:w-[284px] lg:h-[104px] lg:top-[435px] lg:left-[89px] lg:rotate-[6deg]
                 "
@@ -58,7 +58,7 @@ const ContactPage = () => {
               <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 relative z-10 w-full">
                 <div className="w-full">
                   <div className="flex flex-col items-center space-y-8 w-full md:mt-[-220px]">
-                    <div className="flex items-center flex-col space-x-6 md:mt-[10px] mr-[-190px] mb-[-50px] md:ml-[30px]">
+                    <div className="flex items-center flex-col space-x-6 md:mt-[10px] mr-[-240px] mb-[-50px] md:ml-[30px]">
                       <div className="md:w-55 md:h-55 w-32 h-32 rounded-full border-4 border-yellow-400 overflow-hidden">
                         <img
                           src="https://s3-alpha-sig.figma.com/img/cd4b/18e8/6e997e80693617249606bd8795db8ec0?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=L1OMp1YIJy5lKxwGQXm8L-6mJgwvD7kGYHnyT7~-GPvO2qUR-mJnUpfqrcBvzZuikmXGBXJgja25uedfiPgTpBNkw6PONI9fxKdQ~2rIDKkhzHOmuN5kVuddDGB0-KaHeHEfAPQJqDDXjoRaCXBQXbGFClLqj2juiypQlFfzajzuXbaKvQkuUSrojZmqhpZI2R6DMg25W02X0rqccvRzaD~rKBnUUwukdKL4YF8dMTq3v5Jk3FpOlY6iU~FZ~15IFhKuJEbjBYpWxcPIqKYbvSAl5I6gGh-GdYPMu7ybQLBetvAP2Za29Q3V-TxMBOrEWB4O2d2xmzMv3Up4FHxkOw__"
@@ -72,7 +72,7 @@ const ContactPage = () => {
                       </div>
                     </div>
 
-                    <div className="flex items-center flex-col self-end space-y-3 md:mr-[-250px] mt-[12px] mr-[-85px] md:mt-[-10px]">
+                    <div className="flex items-center flex-col self-end space-y-3 md:mr-[-250px] mt-[29px] mr-[-135px] md:mt-[-10px]">
                       <div className="md:w-55 md:h-55 w-32 h-32  rounded-full border-4 border-blue-500 overflow-hidden">
                         <img
                           src="https://s3-alpha-sig.figma.com/img/2517/76a7/c47934b95505556a5d40dfcc24d3aee2?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=g2xGkrFKR1GNkF-ZnlINHifQuDPBJqwR4wxxMUnrbe3spAbkOlLwkjRDwKTM78RX6ZBt4hviv2ClMfPahruWOyk10~fw-lrA087rGMnBjdMvn~UpvpKOQEmHSYHE1hPDcSA8ud0-ym59lPZV5JiXmaBHGIhv18k4K15DJlcGnNjEapm8jm61cJlhjPqBjiHJLr0~fP0QHFMdBaJV53pN1J1LO6MBBuN3f7d2I4EYTGiAKDpLxbHNF1SsdzX2QhpVfMAEl1fTXRczjFrZpC~RYdT8S7GJoM9YV5lRsYJkc2pGO4KVaCliMTB6gbHDINPRwoE7jiBNv18-cxuhTJiCzA__"
@@ -86,7 +86,7 @@ const ContactPage = () => {
                       </div>
                     </div>
 
-                    <div className="flex md:mr-[190px] md:mt-[-225px] md:mb-[0px] mr-[06px] mt-[-155px] mb-[20px] items-center flex-col space-x-6">
+                    <div className="flex md:mr-[190px] md:mt-[-225px] md:mb-[0px] mr-[-70px] mt-[-175px] mb-[20px] items-center flex-col space-x-6">
                       <div className="md:w-55 md:h-55 w-32 h-32  rounded-full border-4 border-green-500 overflow-hidden">
                         <img
                           src="https://s3-alpha-sig.figma.com/img/7f21/5f05/844c0e7d065012026c1c112ec07a03be?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FanQGBhdTQJnz2NkLzmlwl0MVbZjEbP5kPllZ~4cx4rh7FiNuHxVkIr6dW1SAyyjh3AvlJ1Ogs1I4Yrk-wzqRRfZ4Rxe1kmXmjId7V8A9AHur7BBHzqApnDAe2HtkZ~zwRaA9Y6kbQ9EUnHQN8SDSJPhchL50GFrUwRBV2QZ~hbf7-PuIW3qcYupuuGwkQ80PqdCmHk-bPhcoSBLT8c4wy10ylqZ8~1AS~uLTKAiAVcibYb1284YHa71vdvc4Oqe~Cg1oAQkd7F-8T~NXZ-4lOaeVCLmsWzrkL~qX1gMQV6JsG~xUd4wjrveANC38RuuEJv7amKKFs7mxG~bgXus1A__"
