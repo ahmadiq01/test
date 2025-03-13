15966ac694f71df633d939fe51c3d3adf60be1f0
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-03-13 23:31:22 +0500

    added image for broken

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
 import vectorIcon from "../../assets/Group 55.svg";
+import brokenImage from "../../assets/brokenimage.png";
 
 import { useState } from "react";
 
@@ -124,7 +125,7 @@ const ContactPage = () => {
       <div className="w-full">
         <img
           src="https://s3-alpha-sig.figma.com/img/dd4f/9d43/80fbdbdcfb9e32c6aa893ccf48e17ab7?Expires=1742169600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k9rcfgzH4xuSqz6UjTdezXwAqs2L0G~RlYEUo7N-QfujR0e6TsnGOJhjx9Oa7rypnQT2ndP6Pnd2RBR8rxWQj2beg~5sO98JEc-uCbshBZPwzhDCE7S28ei0wavv5iSeQSwzV5pbCc4MaK1BqTBHqLYEHBZRfpq0kcO5sXtzGCBcjWx3GAOO1~DW9KOcKRQe8ZQ2EOGNAzwBODRldnYfpZRGNP-9lPn2kTyB9nqRrTMFsyYWN5W~GDUZB9Ebx71qxGVjnBryVb2hx7xp7lWc3wY7HS8ZVFJFYXxC9h~Q0F6ojN6sX8ircChh3~G8L8hX6HIHXKaNjOCe7h8Wm~dH~w__"
-          alt="Background Img"
+          alt={brokenImage}
           className="w-full h-auto max-h-[440.5px] md:max-h-[881px]"
         />
       </div>
