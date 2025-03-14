43db0ae5354f8c2fb0885b131e49c43231e8360f
Ahmad Qureshi <89626382+ahmadiq01@users.noreply.github.com>
2025-03-15 03:25:14 +0500

    Update header.jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,7 +4,7 @@ import frameLogo from "../../assets/Frame.svg"; // Adjust path if needed
 const Header = () => {
   return (
     <header className=" top-6  w-full p-4">
-      <div className="flex pl-8 pt-4">
+      <div className="flex pr-2 pt-2">
         <a href="home">
         <img src={frameLogo} alt="Logo" className="h-12 w-auto" />
         </a>
